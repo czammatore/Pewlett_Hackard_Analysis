15e84e1403b31947d4f658e398c525e396442381
czammatore <c.zammatore@gmail.com>
2022-02-24 20:33:27 -0500

    updating data

--- schema.sql
+++ schema.sql
@@ -33,18 +33,19 @@ CREATE TABLE salaries (
 );
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
-  dept_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no)
 );
+
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments
