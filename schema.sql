bbdccbf92d7f9ffd4996a8847014c408ae943664
czammatore <c.zammatore@gmail.com>
2022-02-24 21:11:22 -0500

    updating to add new csv

--- schema.sql
+++ schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE dept_emp (
   to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no)
+   PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -49,3 +49,11 @@ CREATE TABLE titles (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
