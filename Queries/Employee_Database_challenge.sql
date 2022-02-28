27b7c09e1f6a003d14a2c527543db71948d9ec58
czammatore <c.zammatore@gmail.com>
2022-02-27 22:44:53 -0500

    updating data

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -23,7 +23,8 @@ WHERE (rt.to_date = '9999-01-01')
 ORDER BY emp_no, to_date DESC;
 
 --Mentorship Eligibility Table
-SELECT e.emp_no,
+--Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
 		e.first_name,
 		e.last_name,
         e.birth_date,
@@ -31,3 +32,13 @@ SELECT e.emp_no,
         de.to_date,
         t.title
 INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles as t
+				ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility;
