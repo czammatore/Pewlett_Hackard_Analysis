b8e343381fd57689bce2bd9bcf738df2ff9d7f77
czammatore <c.zammatore@gmail.com>
2022-02-27 22:28:03 -0500

    adding employee DB code

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,23 @@
-test
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM titles as t
+INNER JOIN employees as e
+	ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
